18a4442e7fb311c5dda54eed24f90973b6accc6a
Gurudutt Suryanarayana <29789603+gnsuryan@users.noreply.github.com>
2021-08-06 11:02:07 +0530

    Create testScript.sh

---
+++ testScript.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+args=("$@")
+# get number of elements
+ELEMENTS=${#args[@]}
+
+# echo each element in array
+# for loop
+for (( i=0;i<$ELEMENTS;i++)); do
+    echo ${args[${i}]}
+done
