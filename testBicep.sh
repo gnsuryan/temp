bd1a388bbe6386bccb97524b384293b9177e054a
Gurudutt Suryanarayana <29789603+gnsuryan@users.noreply.github.com>
2023-09-06 18:31:21 +0530

    Update testBicep.sh

--- testBicep.sh
+++ testBicep.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-echo "Printing environment variables ARG1 and ARG2"
-echo "ARG1=$ARG1"
-echo "ARG2=$ARG2"
+echo "Printing command line arguments"
+echo "arg1=$1"
+echo "arg2=$2"
+
+echo "Printing environment variables ENV1 and ENV2"
+echo "ENV1=$ENV1"
+echo "ENV2=$ENV2"
 now=$(date)
 echo "Script executed at time: $now"
